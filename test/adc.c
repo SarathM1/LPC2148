f792cec2b14bdbb54c1396f85e249b2f72c310bd
Wirewords-PC <sarathm00@gmail.com>
2015-11-02 15:43:11 +0530

    Working

--- test/adc.c
+++ test/adc.c
@@ -3,7 +3,7 @@
 #include "uart0_rx_interrupt.h"
 void adc_init()
 {
-	PINSEL1 |= (1<<24);		// P0.28 as AD0.1
+	PINSEL1 |= (1<<24); 		// P0.28 as  AD0.1
 	AD0CR = (1<<1)|(1<<21)|(1<<8)|(1<<9);
 }
 
@@ -17,12 +17,9 @@ int main()
 	while(1)
 	{
 		AD0CR |= (1<<24);
-		while(!(AD0GDR & (1<<31)));
-		//chn = (AD0GDR & ((1<<24)|(1<<25)|(1<<26)))>>24;
-		val = (AD0DR0 & 0xffc0) >> 6;
+		while(!(AD0GDR & (1<<31))) ;  // Waiting for adc
+		val = (AD0DR1 & 0xFFC0) >> 6;
 		uart_tx_int(val);
-//		uart_tx_char(' ');
-//	   	uart_tx_int(chn);
 		uart_tx_char('\r');
 		delay_ms(500);	
 	}
